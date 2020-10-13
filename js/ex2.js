bd5e43c2e8a8dc4d9a880f883be3082b398610ed
Baron Liu <bbaronliu@gmail.com>
2020-10-13 01:00:34 -0700

    allfixed

--- js/ex2.js
+++ js/ex2.js
@@ -4,17 +4,31 @@ Question 2 JavaScript code
 console.log("Baron's Output from HW 6 Question 2");
 
 const displayText400 = () => {
-    document.getElementsByName('400level').forEach = (ele, idx) => {
-        console.log(ele)
+    let x = function() {
+        four = document.getElementsByTagName("400level");
+    };
+    let y = function() {
+        for (var i=0; i<bold_Items.length; i++)
+        {
+            console.log(x[i]);
+        }
     }
+    y()
 };
 
 displayText400();
 
 const paragraphBoldYellow = () => {
-    document.getElementsByName('submit').forEach = (ele, idx) => {
-        ele.style.fontweight = 'bold';
-        ele.style.fontweight = "yellow";)
+    let x = function() {
+        four = document.getElementsByTagName("D");
+    }
+    let y = function() {
+        for (var i=0; i<bold_Items.length; i++)
+        {
+            x[i].style.fontweight = "bold";
+            x[i].style.fontweight = "yellow";
+        }
+    y()
     }
 };
 
@@ -23,11 +37,11 @@ paragraphBoldYellow();
 
 const addLink = () => {
     var a = document.createElement('a');
-    var linkText = document.createTextNode("College of Business");
-    a.appendChild(linkText);
-    a.title = "College of Business";
-    a.href = "https://www.csulb.edu/college-of-business";
-    document.getElementById("D").appendChild(a)
+    var link = document.createTextNode("This is link"); 
+    a.appendChild(link);
+    a.title = "College of Business"; 
+    a.href = "https://www.csulb.edu/college-of-business"; 
+    document.getElementById("D").appendChild(a);
 };
 
 addLink();
